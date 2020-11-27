5b57fee18d25517b4d5c2e197de36c04ac64d67c
jiayijiayi777 <66906218+jiayijiayi777@users.noreply.github.com>
2020-11-27 14:57:45 +0800

    Add files via upload

---
+++ Open the website and wait 5 seconds to close the website program.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# First run in the terminal: pip install requests
+# Update your Chrome to the latest version
+# Download the latest version at http://chromedriver.storage.googleapis.com/index.html
+# Unzip the downloaded file and put it in the Scripts folder of your python installation directory
+
+import requests  # Call the requests library
+import time  # Call time library
+from selenium import webdriver  # Invoke Chrome Drive
+
+start_url = input('Please enter the URL, including http:')  # Set website link
+driver = webdriver.Chrome()  # Use Chrome Drive
+driver.get(url=start_url)  # open the Web page
+time.sleep(3)  # Wait 3 seconds
+driver.close()  # Close page
